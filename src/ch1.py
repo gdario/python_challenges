70c43c6eb6cfb7bbe16633f57c5fe479697f1fed
Giovanni d'Ario <gda@Bimbo.home>
2025-12-14 13:47:24 +0100

    Add solution 7a

--- src/ch1.py
+++ src/ch1.py
@@ -194,6 +194,24 @@ def number_to_text2(n):
     return result
 
 
+def from_roman_number(roman_number):
+    value = 0
+    last_digit_value = 0
+    value_map = {"I": 1, "V": 5, "X": 10, "L": 50,
+                 "C": 100, "D": 500, "M": 1000}
+    # for i in range(len(roman_number) - 1, -1, -1):
+    #     roman_digit = roman_number[i]
+    for roman_digit in reversed(roman_number):
+        digit_value = value_map[roman_digit]
+        add_mode = digit_value >= last_digit_value
+        if add_mode:
+            value += digit_value
+            last_digit_value = digit_value
+        else:
+            value -= digit_value
+    return value
+
+
 def roman_to_decimal(roman_number):
     # Repetition rule: no more than 3 consecutive identical digits
     # Subtraction rules
